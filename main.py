c582dee915fdd99ec2839671a5df198f409b3242
Manoel Neto <manoelcontatoneto@gmail.com>
2026-03-06 20:09:01 -0300

    feat: modulo captura de tela - desenvolvimento fluxo

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import mss
-import cv2
-import numpy as np
-
-
-def captura_tela():
-     with mss.mss() as sct:
-        monitor = sct.monitors[1]  # Captura a tela principal
-        frame = np.array(sct.grab(monitor))  # Captura a tela e converte para um array numpy
-        return cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)  # Converte de BGRA para BGR
+from capture.captura_tela import captura_tela
 
 if __name__ == "__main__":
-   frame = captura_tela()
-   cv2.imshow("Tela Capturada", frame)  # Exibe a tela capturada
-   cv2.waitKey(0)  # Aguarda até que uma tecla seja pressionada
-   cv2.destroyAllWindows()  # Fecha a janela exibida
+     captura_tela()
+   
 
